08242cdaeea0a13673ee38d00bb5d4e805f1d520
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2024-08-15 13:50:26 -0300

    CobrosWap class, payment link y response dentro de modelo.

--- cobroswap/payment_link.py
+++ cobroswap/payment_link.py
@@ -1,32 +1,21 @@
-from typing import Literal
-import os
+import logging
 
 import requests
 from requests import Response
+from cobroswap.models import CobrosWapJSON, ResponseCobrosWap
 
-CobroswapCurrency = Literal["UYU"]
+logger = logging.getLogger(__name__)
 
 def url_create_payment_link() -> str:
     """ FIXME: Esto es genérico?"""
     return "https://cobroswap.com/api/banking/createExternalPayment"
 
-def _get_json_create_payment_link(*, amount: float, currency: CobroswapCurrency) -> dict:
-    """ TODO: Ver `concept` y `companyId`."""
-    return {
-        "amount": amount,
-        "currency": currency,                                   # FIXME: Que monedas acepta?
-        "concept": "Membresia 3 meses",                         # FIXME: Hardcodeado.
-        "companyId": os.getenv("COMPANY_ID")                    # FIXME: Dato sensible?
-    }
-
-def response_create_payment_link(*, amount: float, currency: CobroswapCurrency) -> Response:
+def response_create_payment_link(*, cobroswap_json: CobrosWapJSON) -> Response:
     url = url_create_payment_link()
-    json_ = _get_json_create_payment_link(amount=amount, currency=currency)
-    return requests.post(url=url, json=json_)
+    return requests.post(url=url, json=cobroswap_json.model_dump())
 
-def create_payment_link(*, amount: float, currency: CobroswapCurrency) -> str | None:
-    r = response_create_payment_link(amount=amount, currency=currency)
-    if not(200 <= r.status_code <= 299):
-        return None
-    url_payment = r.json()["paymentLink"]["url"]
-    return url_payment
+def create_payment_link(*, cobroswap_json: CobrosWapJSON) -> ResponseCobrosWap | None:
+    r = response_create_payment_link(cobroswap_json=cobroswap_json)
+    if r.status_code == 201:
+        return ResponseCobrosWap(**r.json())
+    logger.warning(f"create_payment_link: status_code={r.status_code} | text={r.text}")
